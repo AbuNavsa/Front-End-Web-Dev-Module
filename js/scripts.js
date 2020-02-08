6ce6e6375a0689a940285841a5fdf4d393e08a88
AbuNavsa <abu_navsa@outlook.com>
2020-02-08 16:56:22 +0000

    Added Typing Javascript effect to 'Untamed' on home

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 console.log("hello");
+
+var i = 0;
+var coverTxt = "Untamed";
+var coverSpeed = 175;
+
+function typeWriter() {
+  if (i < coverTxt.length) {
+    document.getElementById("coverTyped").innerHTML += coverTxt.charAt(i);
+    i++;
+    setTimeout(typeWriter, coverSpeed,);
+  }
+
+
+}
