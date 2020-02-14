76d6fe46d8031e7730b2b1f2f0eb75ab190d2ca7
AbuNavsa <abu_navsa@outlook.com>
2020-02-14 12:43:45 +0000

    added javascript to make button scroll to top of page feature

--- js/scripts.js
+++ js/scripts.js
@@ -46,3 +46,10 @@ window.onscroll = function() {
 
     }
 }
+
+//To Top Button ---------------------------
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
