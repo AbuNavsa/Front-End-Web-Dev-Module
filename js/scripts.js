cebb5410734f9afb92be283cc39465c51ae0e021
AbuNavsa <abu_navsa@outlook.com>
2020-04-22 11:10:31 +0100

    clean up complete, alt tags filled and code validation and correct 2 space indentation used

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,15 @@ let coverTxt = "Untamed";
 let coverSpeed = 1;
 
 function typeWriter() {
-  setTimeout(function(){
-      //deferred onload
-      if (i < coverTxt.length) {
-        document.getElementById("coverTyped").innerHTML += coverTxt.charAt(i);
-        i++;
-        setTimeout(typeWriter, coverSpeed);
-      }
-    }, 250);
-  }
-
+  setTimeout(function () {
+    //deferred onload
+    if (i < coverTxt.length) {
+      document.getElementById("coverTyped").innerHTML += coverTxt.charAt(i);
+      i++;
+      setTimeout(typeWriter, coverSpeed);
+    }
+  }, 250);
+}
 
 
 //Nav Bar Colour Change Past navScrollPoint
@@ -30,26 +29,25 @@ const header_height = header.offsetHeight
 
 
 const addScrollH = () => header.classList.add("header-scroll")
-const removeScrollH= () => header.classList.remove("header-scroll")
+const removeScrollH = () => header.classList.remove("header-scroll")
 
 const logo = document.querySelector(".logo")
 const addScrollL = () => logo.classList.add("logo-scroll")
-const removeScrollL= () => logo.classList.remove("logo-scroll")
+const removeScrollL = () => logo.classList.remove("logo-scroll")
 
 const nav = document.querySelector(".navbar-inner")
 const addScrollN = () => nav.classList.add("navbar-inner-scroll")
-const removeScrollN= () => nav.classList.remove("navbar-inner-scroll")
+const removeScrollN = () => nav.classList.remove("navbar-inner-scroll")
 
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   scrollpos = window.scrollY;
 
   if (scrollpos >= header_height) {
     addScrollH()
     addScrollL()
     addScrollN()
-}
-  else {
+  } else {
     removeScrollH()
     removeScrollL()
     removeScrollN()
@@ -57,22 +55,18 @@ window.addEventListener('scroll', function() {
 })
 
 
-
 //Navigation bar button-------------------
 
-    let mobMenu= document.getElementById("navbar");
-    let menuBg= document.querySelector("header");
+let mobMenu = document.getElementById("navbar");
+let menuBg = document.querySelector("header");
 
 
-document.querySelector("#hbg-menu").addEventListener("click", function(){
-        mobMenu.classList.toggle("nav-bar");
-        mobMenu.classList.toggle("navbar-show");
+document.querySelector("#hbg-menu").addEventListener("click", function () {
+  mobMenu.classList.toggle("nav-bar");
+  mobMenu.classList.toggle("navbar-show");
 
 
-
-
-
-    });
+});
 
 
 //To Top Button ---------------------------
@@ -89,29 +83,29 @@ var quoteSlideIndex = 1;
 showQuoteSlides(quoteSlideIndex);
 
 function incSlides(n) {
-    showQuoteSlides(quoteSlideIndex += n );
+  showQuoteSlides(quoteSlideIndex += n);
 }
 
 function currentQuoteSlides(n) {
-    showQuoteSlides(quoteSlideIndex = n );
+  showQuoteSlides(quoteSlideIndex = n);
 }
 
 function showQuoteSlides(n) {
-    var i;
-    var quoteSlides = document.getElementsByClassName('quote-slides');
-    
-    if(n > quoteSlides.length) {
-        quoteSlideIndex = 1
-    }
-    if(n < 1) {
-        quoteSlideIndex = quoteSlides.length
-    }
-    for (i = 0; i < quoteSlides.length; i++) {
-        quoteSlides[i].style.display="none";
-    }
-    
-    quoteSlides[quoteSlideIndex-1].style.display = "block";
-    
+  var i;
+  var quoteSlides = document.getElementsByClassName('quote-slides');
+
+  if (n > quoteSlides.length) {
+    quoteSlideIndex = 1
+  }
+  if (n < 1) {
+    quoteSlideIndex = quoteSlides.length
+  }
+  for (i = 0; i < quoteSlides.length; i++) {
+    quoteSlides[i].style.display = "none";
+  }
+
+  quoteSlides[quoteSlideIndex - 1].style.display = "block";
+
 }
 
 let nextQuote = document.querySelector('.next-q');
@@ -119,13 +113,9 @@ let prevQuote = document.querySelector('.previous-q');
 
 
 nextQuote.addEventListener('click', function (event) {
-   incSlides(1);
+  incSlides(1);
 }, false);
 
 prevQuote.addEventListener('click', function (event) {
-   incSlides(-1);
+  incSlides(-1);
 }, false);
-
-
-
-
